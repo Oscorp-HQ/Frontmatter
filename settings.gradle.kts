da8a559e679ccdb4e32b89515acd785b9dc98dd6
AiVolf <abrar@quashbugs.com>
2025-06-25 01:59:25 +0530

    fix: adding new settings requirement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "frontmatter"
 
+pluginManagement {
+  repositories {
+    // still need the Plugin Portal for plugins
+    gradlePluginPortal()
+    // most artifacts live here
+    mavenCentral()
+    // but grgit-core:4.1.0 is only on Appodeal’s public Artifactory
+    maven {
+      url = uri("https://artifactory.appodeal.com/appodeal-public/")
+      content {
+        includeGroup("org.ajoberstar.grgit")
+      }
+    }
+  }
+}
